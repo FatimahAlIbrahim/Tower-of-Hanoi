6f97c00f9ee88cf301140e88521200ed995ead67
Fatimah-Al-Ibrahim <Fatimah.Al.Ibrahim.1997@gmail.com>
2020-12-26 19:56:29 +0300

    added the mode selection functionality in JavaScript file and modified html and CSS files a little

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+
+    var mode = "3 disks";
+
+    // on click for the game mode selection buttons
+    $(".mode").on("click",function(){
+        // remove the "selected-node" class from all the buttons with class "mode"
+        $(".mode").removeClass("selected-mode");
+        // add the "selected-mode" to the clicked button
+        $(this).addClass("selected-mode");
+        // add the clicked button text to the mode variable
+        mode = $(this).text();
+    });
+
+    // on click for the play button
+    $("#play").on("click",function(){
+        // get the number of disks from the mode variable
+        var diskNumber = Number(mode[0]);
+        // calculate the height for the disks
+        var height = 100 / diskNumber;
+        // for loop for creating the disks
+        for (let index = 1; index <= diskNumber; index++) {
+            // calculate the width for the disk
+            var width = (100 / diskNumber) * index;
+            // append the disk to the first box
+            $("#box-A").append("<div class='disk"+index+"' style='width: "+width+"%; height: "+height+"%' ></div>")
+        }
+        // hide the mode selection area
+        $("#mode-selection").hide();
+        // show the game to the player
+        $("#in-game").show();
+    });
+
+
+});
